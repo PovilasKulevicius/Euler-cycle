edcb5317f6de76c0514cc7354c05e1b0932ddaf6
Povilas Kulevičius <kulevicius.povilas@gmail.com>
2019-05-14 23:14:59 +0300

    fixed start vertex number

--- src/Main.java
+++ src/Main.java
@@ -24,19 +24,21 @@ class Main {
                 }
             }
         }
-        int countOdd = checkIfEuler(gretimumoMatrica, virsuniuSkaicius);
-        switch (countOdd) {
+        //vertexInfo[0] - odd vertex number
+        //vertexInfo[1] - start vertex number
+        int[] vertexInfo = checkIfEuler(gretimumoMatrica, virsuniuSkaicius);
+        switch (vertexInfo[0]) {
             case 0:
                 System.out.println("Graph has an Euler circuit");
-                Euler(gretimumoMatrica, virsuniuSkaicius);
+                Euler(gretimumoMatrica, virsuniuSkaicius, vertexInfo[1]);
                 break;
             case 1:
                 System.out.println("Graph has an Euler path");
-                Euler(gretimumoMatrica, virsuniuSkaicius);
+                Euler(gretimumoMatrica, virsuniuSkaicius, vertexInfo[1]);
                 break;
             case 2:
                 System.out.println("Graph has an Euler path");
-                Euler(gretimumoMatrica, virsuniuSkaicius);
+                Euler(gretimumoMatrica, virsuniuSkaicius, vertexInfo[1]);
                 break;
             default:
                 System.out.println("The graph is not Eulerian");
@@ -44,25 +46,29 @@ class Main {
         }
     }
 
-    private static int checkIfEuler(int[][] gretimumoMatrica, int virsuniuSkaicius) {
-        int countVertex = 0;
-        int countOdd = 0;
+    private static int[] checkIfEuler(int[][] gretimumoMatrica, int virsuniuSkaicius) {
+        int[] vertexInfo = new int[2];
+        int temp;
+        vertexInfo[1] = 0;
         for (int i = 0; i < virsuniuSkaicius; i++) {
+            temp = 0;
             for (int j = 0; j < virsuniuSkaicius; j++) {
                 if (gretimumoMatrica[i][j] == 1)
-                    countVertex++;
+                    temp++;
             }
-            if (countVertex % 2 != 0)
-                countOdd++;
+            if (temp % 2 != 0) {
+                vertexInfo[0]++;
+                vertexInfo[1] = i;
+            }
         }
-        return countOdd;
+        return vertexInfo;
     }
 
 
-    private static void Euler(int[][] gretimumoMatrica, int virsuniusSkaicius) {
+    private static void Euler(int[][] gretimumoMatrica, int virsuniusSkaicius, int startVertex) {
         ArrayList<Integer> STEKAS = new ArrayList<>();
         ArrayList<Integer> CIKLAS = new ArrayList<>();
-        int v = 0;
+        int v = startVertex;
         Integer u = null;
         STEKAS.add(v);
         while (!STEKAS.isEmpty()) {
